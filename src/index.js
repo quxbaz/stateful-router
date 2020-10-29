1e37088ba19d5b00d6cb11f62e2ee1ecb17ae979
David Yeung <quxbaz@gmail.com>
2020-10-29 02:55:14 -0600

    Removed test code.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import Router from './Router'
 import Route from './Route'
 
-console.log('stateful-router/index.js')
-window.addEventListener('keydown', (event) => {
-  if (event.key === 'a')
-    throw 'stateful-router ERROR'
-})
-
 export {
   Router,
   Route,
