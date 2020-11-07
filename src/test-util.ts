ca623ba12276bbeb8acc6002fc77f59feddbedb0
David Yeung <quxbaz@gmail.com>
2020-11-07 02:13:06 -0700

    Types for test-util.

--- src/test-util.ts
+++ src/test-util.ts
@@ -1,15 +1,16 @@
+import {ReactElement} from 'react'
 import {render} from 'react-dom'
 
-const nodeTextIs = (component, text) => {
+const nodeTextIs = (component:ReactElement, text:string) => {
   const div = document.createElement('div')
   render(component, div)
   expect(div.textContent).toBe(text)
 }
 
-const nodeIsEmpty = (component) => {
+const nodeIsEmpty = (component:ReactElement) => {
   const div = document.createElement('div')
   render(component, div)
-  expect(div.textContent).toNotExist()
+  expect(div.textContent).toBe('')
 }
 
 export {
